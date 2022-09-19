937851bda2547af3751346bcdd8bf71e02c94877
Bruno Barros <brunowibarros12@gmail.com>
2022-09-18 21:50:23 -0300

    Processamento do menu

--- src/app/App.java
+++ src/app/App.java
@@ -7,8 +7,8 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Register> reg = new ArrayList();
-        
+        ArrayList<Register> list = new ArrayList();
+
         boolean controller = true;
         // MENU USER //
         System.out.println("Welcome to Events!");
@@ -18,19 +18,53 @@ public class App {
             System.out.println("[3]- OUT");
             System.out.print("TYPE AN OPTION:");
             int opn = sc.nextInt();
+            sc.nextLine();
             // PROCESSING //
             switch (opn) {
+                // SIGN UP //
                 case 1: {
-                    System.out.println("====== REGISTER A USER ======");
+                    System.out.println("========== SIGN UP ==========");//29
                     System.out.println("type ur email:");
                     String email = sc.nextLine();
-                    System.out.println("no a password:");
+                    System.out.print("now a password:");
                     String psw = sc.nextLine();
+
                     Register user = new Register();
-                    
+                    user.setEmail(email);
+                    user.setPsw(psw);
+                    list.add(user);
+
+                    System.out.println("-> registered successfully <-");//28
+                    System.out.println("==============================");//29                   
                     break;
                 }
                 case 2: {
+                    System.out.println("============ LOGIN ===========");
+                    System.out.println("ur email:");
+                    String email = sc.nextLine();
+                    System.out.print("ur password:");
+                    String psw = sc.nextLine();
+                    
+                    // VERIFICATION OF EMAIL //
+                    boolean truLogin = false;
+                    for (Register user : list) {
+
+                        String uEmail = user.getEmail();
+                        String uPsw = user.getPsw();
+
+                        boolean truEmail = email.equals(uEmail);
+                        boolean truPsw = psw.equals(uPsw);
+
+                        if (truEmail && truPsw) {
+                            truLogin = true;
+                            break;
+                        }
+                    }
+                    if (!truLogin) {
+                        System.out.println("Email and Password dont match!");
+                    } else {
+                        System.out.println("Successfully loaded!!!");
+                    }
                     break;
                 }
                 case 3: {
--- src/app/Register.java
+++ src/app/Register.java
@@ -3,4 +3,20 @@ package app;
 
 public class Register {
  String email,psw;   
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPsw() {
+        return psw;
+    }
+
+    public void setPsw(String psw) {
+        this.psw = psw;
+    }
 }
