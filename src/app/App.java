0a96a0e90d67448cd4992bac6855df03435c44a0
Bruno Barros <brunowibarros12@gmail.com>
2022-09-18 22:49:35 -0300

    criação da homepage

--- src/app/App.java
+++ src/app/App.java
@@ -5,24 +5,27 @@ import java.util.Scanner;
 
 public class App {
 
+    public static Scanner sc = new Scanner(System.in);
+    public static Register loggedUser = null;
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
         ArrayList<Register> list = new ArrayList();
 
         boolean controller = true;
         // MENU USER //
-        System.out.println("Welcome to Events!");
+        System.out.println("     Welcome to Events!      ");
         while (controller) {
+            System.out.println("=========== MENU ============");
             System.out.println("[1]- REGISTER A NEW USER");
             System.out.println("[2]- LOGIN");
-            System.out.println("[3]- OUT");
+            System.out.println("[0]- LOGOUT");
             System.out.print("TYPE AN OPTION:");
-            int opn = sc.nextInt();
-            sc.nextLine();
+            String opn = sc.nextLine();
             // PROCESSING //
             switch (opn) {
                 // SIGN UP //
-                case 1: {
+                case "1": {
                     System.out.println("========== SIGN UP ==========");//29
                     System.out.println("type ur email:");
                     String email = sc.nextLine();
@@ -35,17 +38,18 @@ public class App {
                     list.add(user);
 
                     System.out.println("-> registered successfully <-");//28
-                    System.out.println("==============================");//29                   
+                    System.out.println("==============================");//29 
+                    homePage();
                     break;
                 }
-                case 2: {
+                case "2": {
                     System.out.println("============ LOGIN ===========");
                     System.out.println("ur email:");
                     String email = sc.nextLine();
                     System.out.print("ur password:");
                     String psw = sc.nextLine();
-                    
-                    // VERIFICATION OF EMAIL //
+
+                    // EMAIL VERIFICATION  //
                     boolean truLogin = false;
                     for (Register user : list) {
 
@@ -57,6 +61,7 @@ public class App {
 
                         if (truEmail && truPsw) {
                             truLogin = true;
+                            loggedUser = user;
                             break;
                         }
                     }
@@ -64,10 +69,11 @@ public class App {
                         System.out.println("Email and Password dont match!");
                     } else {
                         System.out.println("Successfully loaded!!!");
+                        homePage();
                     }
                     break;
                 }
-                case 3: {
+                case "0": {
                     controller = false;
                     System.out.println("Right,see U!");
                     break;
@@ -81,4 +87,39 @@ public class App {
 
     }
 
+    public static void homePage() {
+        boolean controller = true;
+        while (controller) {
+            System.out.println("======= UR HOMEPAGE =========");
+            System.out.println("[1]- ADD NEW EVENT");
+            System.out.println("[2]- DELETE AN EVENT");
+            System.out.println("[3]- EVENTS YOU CONFIRMED");
+            System.out.println("[4]- EVENTS YOU DIDN'T CONFIRM");
+            System.out.println("[5]- LIST ALL EVENTS");
+            System.out.println("[0]- LOGOUT");
+            System.out.print("Type an option:");
+            String opn = sc.nextLine();
+
+            switch (opn) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "0":
+                    controller = false;
+                    System.out.println("Right,see U!");
+                    break;
+                default: {
+                    System.out.println("You need to enter a valid number!");
+                }
+            }
+
+        }
+    }
 }
